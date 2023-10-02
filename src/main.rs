48a441e1a11afc6e820da8e4d1edc59c4a909150
Dhawal CL <dhawal@codelinter.com>
2023-10-02 08:15:57 +0530

    pull available version

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-#![warn(clippy::pedantic, rust_2018_idioms, clippy::all)]
-#![allow(
-    clippy::enum_variant_names,
-    clippy::large_enum_variant,
-    clippy::module_name_repetitions,
-    clippy::similar_names
-)]
-
+mod actions;
+mod alias;
+mod available_versions;
 mod entry_interface;
 
 #[macro_use]
+mod std_system_structure;
 mod ll_int;
+mod version_std;
 
 fn main() {
     env_logger::init();
