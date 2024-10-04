8caac0dba41c734e66d33536c04e2a86935f6451
Dhawal CL <dhawal@codelinter.com>
2024-10-04 18:32:36 +0530

    fix for windows

--- src/fs.rs
+++ src/fs.rs
@@ -6,6 +6,24 @@ pub fn symlink_dir<P: AsRef<Path>, U: AsRef<Path>>(from: P, to: U) -> std::io::R
     Ok(())
 }
 
+#[cfg(windows)]
+pub fn symlink_dir<P: AsRef<Path>, U: AsRef<Path>>(from: P, to: U) -> std::io::Result<()> {
+    junction::create(from, to)?;
+    Ok(())
+}
+
+#[cfg(windows)]
+pub fn remove_symlink_dir<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
+    std::fs::remove_dir(path)?;
+    Ok(())
+}
+
+#[cfg(unix)]
+pub fn remove_symlink_dir<P: AsRef<Path>>(path: P) -> std::io::Result<()> {
+    std::fs::remove_file(path)?;
+    Ok(())
+}
+
 pub fn shallow_read_symlink<P: AsRef<Path>>(path: P) -> std::io::Result<std::path::PathBuf> {
     std::fs::read_link(path)
 }
